23a4082a0164f686e6ae3e4a3c28fd3dac89d322
ryazanovs <sergey.ryazanov@sfera.zone>
2024-08-13 12:23:54 +0300

    feat(main): Add easy server http

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	// Определяем функцию-обработчик HTTP запросов
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		// Отправляем ответ 'Hello, World!' клиенту
+		fmt.Fprintf(w, "Hello, World!")
+	})
+
+	// Настраиваем сервер для прослушивания порта 8080
+	fmt.Println("Server is running on http://localhost:8085")
+	if err := http.ListenAndServe("0.0.0.0:8085", nil); err != nil {
+		fmt.Println(err)
+	}
 }
---
+++ go.mod
@@ -0,0 +1,3 @@
+module easyserver
+
+go 1.21.4
